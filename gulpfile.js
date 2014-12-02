7e8dd669854ab314a6809d829b550a9726174dd1
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-12-02 20:24:37 +0000

    Adding a jekyll build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 var browserSync = require('browser-sync');
-var cp = require('child_process');
+var spawn = require('child_process').spawn;
+
+gulp.task('build', function() {
+    return spawn('jekyll', ['build']);
+});
 
 gulp.task('sass', function() {
     return gulp.src('_sass/main.scss')
